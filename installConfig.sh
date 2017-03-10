aea94a7d27b8b9538e69a899b0020ee0cc6bbf72
Hakchin Kim <hakchin@users.noreply.github.com>
2017-03-10 12:41:24 +0900

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -9,46 +9,46 @@
 #. Cloud Storage and Virtual Environment
 
 #brew cask install xquartz
+#brew cask install hyperdock
+#brew cask install teamviewer
 brew cask install dropbox
 #brew cask install virtualbox
 brew cask install virtualbox-extension-pack
 brew cask install piezo
-#brew cask install hyperdock
-#brew cask install teamviewer
 
 #. browser
 brew cask install google-chrome
 brew cask install firefox
 
 #. development
+#brew install pgcli
+#brew cask install pgadmin3
 brew cask install github-desktop
 brew cask install iterm2
 brew cask install java
 brew install uncrustify
 brew install wget
-#brew install pgcli
-#brew cask install pgadmin3
 brew cask install google-cloud-sdk
 brew install bash
 brew install bash-git-prompt
 
 #. editor
-brew cask install atom
+#brew cask install atom
 #brew cask install brackets
-brew cask install mactex
-brew cask install texpad
 #brew cask install eclipse-java
-brew cask install evernote
 #brew cask install inkscape
 #brew cask install gimp
+brew cask install mactex
+brew cask install texpad
+brew cask install evernote
 brew install vim --with-lua --with-override-system-vi
 brew install macvim --with-cscope --with-lua
-brew cask install adobe-digital-editions
+#brew cask install adobe-digital-editions
 brew cask install adobe-creative-cloud
 
 #.. emacs
-brew install emacs --HEAD --use-git-head  --with-gnutls --with-rsvg --with-imagemagick --srgb --with-modules
-brew install jade
-brew install suggest-emacs
+#brew install emacs --HEAD --use-git-head  --with-gnutls --with-rsvg --with-imagemagick --srgb --with-modules
+#brew install jade
+#brew install suggest-emacs
 
 
